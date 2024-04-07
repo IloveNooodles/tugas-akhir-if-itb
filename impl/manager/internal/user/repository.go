b0b37bdc3d6d05004afd01c1ffe06be48af6c23c
IloveNooodles <mgarebaldhie80@gmail.com>
2024-04-08 02:51:03 +0700

    feat: user

--- impl/manager/internal/user/repository.go
+++ impl/manager/internal/user/repository.go
@@ -3,6 +3,7 @@ package user
 import (
 	"context"
 
+	"github.com/google/uuid"
 	"github.com/jmoiron/sqlx"
 	"github.com/sirupsen/logrus"
 )
@@ -25,19 +26,35 @@ func (r *Repository) Create(ctx context.Context, u User) (User, error) {
 	err := r.DB.GetContext(ctx, &user, q, u.Name, u.Email, u.Password)
 
 	if err != nil {
-		r.Logger.Errorf("error when creating users detail: %v, err: %s", user, err)
+		r.Logger.Errorf("error when creating users %v, err: %s", user, err)
 		return user, err
 	}
 
 	return user, nil
 }
 
-func (r *Repository) GetByID() (User, error) {
+func (r *Repository) GetByID(ctx context.Context, id uuid.UUID) (User, error) {
 	user := User{}
+	q := `SELECT * FROM users WHERE id = $1`
+	err := r.DB.GetContext(ctx, &user, q, id)
+
+	if err != nil {
+		r.Logger.Errorf("error when get users with id: %s, err: %s", id, err)
+		return user, err
+	}
+
 	return user, nil
 }
 
-func (r *Repository) GetByEmail() (User, error) {
+func (r *Repository) GetByEmail(ctx context.Context, email string) (User, error) {
 	user := User{}
+	q := `SELECT * FROM users WHERE email = $1`
+	err := r.DB.GetContext(ctx, &user, q, email)
+
+	if err != nil {
+		r.Logger.Errorf("error when get users with id: %s, err: %s", email, err)
+		return user, err
+	}
+
 	return user, nil
 }
---
+++ impl/manager/internal/user/usecase.go
@@ -0,0 +1,30 @@
+package user
+
+import (
+	"context"
+
+	"github.com/google/uuid"
+	"github.com/sirupsen/logrus"
+)
+
+type Usecase struct {
+	Logger *logrus.Logger
+	Repo   *Repository
+}
+
+func NewUsecase(l *logrus.Logger, r *Repository) *Usecase {
+	return &Usecase{
+		Logger: l,
+		Repo:   r,
+	}
+}
+
+func (u *Usecase) Create(ctx context.Context, user User) (User, error) {
+	return u.Repo.Create(ctx, user)
+}
+func (u *Usecase) GetByID(ctx context.Context, id uuid.UUID) (User, error) {
+	return u.Repo.GetByID(ctx, id)
+}
+func (u *Usecase) GetByEmail(ctx context.Context, email string) (User, error) {
+	return u.Repo.GetByEmail(ctx, email)
+}
