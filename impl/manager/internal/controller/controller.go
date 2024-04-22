cba7b488da75ec66af2b624afec72a570a4a0b3e
IloveNooodles <mgarebaldhie80@gmail.com>
2024-04-22 12:18:52 +0700

    feat: progress

--- impl/manager/internal/controller/controller.go
+++ impl/manager/internal/controller/controller.go
@@ -4,14 +4,17 @@ import (
 	"fmt"
 
 	"github.com/sirupsen/logrus"
+	"k8s.io/client-go/kubernetes"
+	v1 "k8s.io/client-go/kubernetes/typed/core/v1"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/tools/clientcmd/api"
 )
 
 type KubernetesController struct {
-	Logger *logrus.Logger
-	Config clientcmd.ClientConfig
+	Logger    *logrus.Logger
+	Config    clientcmd.ClientConfig
+	ClientSet *kubernetes.Clientset
 }
 
 func New(l *logrus.Logger) *KubernetesController {
@@ -25,11 +28,26 @@ func New(l *logrus.Logger) *KubernetesController {
 		l.Fatalf("error when reading raw cfg err: %s", err)
 	}
 
+	restCfg, err := cfg.ClientConfig()
+	if err != nil {
+		l.Fatalf("error when reading rest cfg err: %s", err)
+	}
+
+	clienset, err := kubernetes.NewForConfig(restCfg)
+	if err != nil {
+		l.Fatalf("error when creating clientset err: %s", err)
+	}
+
 	l.Infof("success connected to context: %s", rawCfg.CurrentContext)
 	return &KubernetesController{
-		Logger: l,
-		Config: cfg,
+		Logger:    l,
+		Config:    cfg,
+		ClientSet: clienset,
 	}
+}
+
+func (k *KubernetesController) GetConfig() clientcmd.ClientConfig {
+	return k.Config
 }
 
 func (k *KubernetesController) GetRawConfig() (api.Config, error) {
@@ -51,6 +69,10 @@ func (k *KubernetesController) GetRestConfig() (*rest.Config, error) {
 	return restCfg, nil
 }
 
+func (k *KubernetesController) GetNodeInterface() v1.NodeInterface {
+	return k.ClientSet.CoreV1().Nodes()
+}
+
 func (k *KubernetesController) SwitchCluster(ctx string) error {
 	configAccess := k.Config.ConfigAccess()
 	rawConfig, err := k.Config.RawConfig()
@@ -60,7 +82,13 @@ func (k *KubernetesController) SwitchCluster(ctx string) error {
 		return err
 	}
 
-	k.Logger.Infof("current context %s", ctx)
+	currentContext := rawConfig.CurrentContext
+	k.Logger.Infof("current context %s", currentContext)
+
+	if currentContext == ctx {
+		return nil
+	}
+
 	err = validateContext(&rawConfig, ctx)
 	if err != nil {
 		err := fmt.Errorf("error %s, validating kubeconfig", err.Error())
@@ -77,9 +105,23 @@ func (k *KubernetesController) SwitchCluster(ctx string) error {
 	}
 
 	cfg := clientcmd.NewNonInteractiveClientConfig(rawConfig, ctx, &clientcmd.ConfigOverrides{}, configAccess)
+	restCfg, err := cfg.ClientConfig()
+	if err != nil {
+		err := fmt.Errorf("error when getting rest config: %s", err)
+		k.Logger.Error(err)
+		return err
+	}
+
+	clienset, err := kubernetes.NewForConfig(restCfg)
+	if err != nil {
+		err := fmt.Errorf("error when creating clientset err: %s", err)
+		k.Logger.Error(err)
+		return err
+	}
+
+	k.ClientSet = clienset
 	k.Config = cfg
+	k.Logger.Infof("context switched to: %s", ctx)
 
 	return nil
 }
-
-func (k *KubernetesController) CreateDeployment(replica int, label map[string]string) {}
