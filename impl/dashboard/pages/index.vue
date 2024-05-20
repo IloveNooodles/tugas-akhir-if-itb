74accf2993273aa8084e1dbea3645eea92276d29
IloveNooodles <mgarebaldhie80@gmail.com>
2024-05-21 01:43:44 +0700

    feat: update dashboard

--- impl/dashboard/pages/index.vue
+++ impl/dashboard/pages/index.vue
@@ -8,5 +8,38 @@ definePageMeta({
   <UContainer class="flex-1">
     <h1 class="text-center">Dashboard</h1>
     <UDivider />
+    <div class="mt-4">
+      <h2>Quick Actions</h2>
+      <div class="flex gap-2">
+        <UButton
+          label="Remote Deployment"
+          icon="i-heroicons-document-plus"
+          class="mt-2 max-w-fit self-end"
+          to="/remote-deployment"
+          size="xl"
+        />
+        <UButton
+          label="See Devices"
+          icon="i-heroicons-document-plus"
+          class="mt-2 max-w-fit self-end"
+          to="/devices"
+          size="xl"
+        />
+        <UButton
+          label="See Groups"
+          icon="i-heroicons-document-plus"
+          class="mt-2 max-w-fit self-end"
+          to="/groups"
+          size="xl"
+        />
+        <UButton
+          label="How to register devices"
+          icon="i-heroicons-document-plus"
+          class="mt-2 max-w-fit self-end"
+          to="/faq/how-to-add-devices"
+          size="xl"
+        />
+      </div>
+    </div>
   </UContainer>
 </template>
